52316766de4fdd6deda3acf34884a94d2b6eed18
AdamAlkaz <alkhazra@uwindsor.ca>
2016-11-13 18:16:55 -0500

    Insertion Sort

--- Lab7/insertion_sort.c
+++ Lab7/insertion_sort.c
@@ -1,7 +1,7 @@
 //insetion sort algorithm to be included into other programs
 
-//#include <stdio.h>
-//#include <stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 
 void insertSort (int *Array, int size){
@@ -19,7 +19,7 @@ void insertSort (int *Array, int size){
 
 //Testing
 
-/*
+
 void printArray (int *array, int size){
 	for (int i = 0; i < size; i++){
 		printf("%d   ", array[i]);
@@ -27,9 +27,13 @@ void printArray (int *array, int size){
 }
 
 int main(){
-	int a [] = {5,4,3,2,1};
+	int *a = (int*) malloc(5 * sizeof (int));
+	for (int i = 5 ; i > 0 ; i--){
+		a[i - 1] = i;
+	}	
 	insertSort (a, 5);
 	printArray (a, 5);
+	free(a);
 	return 0;
 }
-*/
+
