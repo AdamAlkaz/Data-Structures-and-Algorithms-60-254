8fc43b4b010e6f0a0543e5f80a43f0dbf9c8167e
AdamAlkaz <alkhazra@uwindsor.ca>
2016-11-13 17:03:43 -0500

    Circle Queue

--- Circle Queue in C/circle_queue.c
+++ Circle Queue in C/circle_queue.c
@@ -12,25 +12,33 @@ struct queue{
 	int front;
 };
 
+//(*Struct_Pointer).Element is the same is Struct_Pointer -> Element
+
 void makeQueue (struct queue *circQueue, int size){
-	(*circQueue).array = (int*) malloc (size * sizeof(int));
-	(*circQueue).size = size;
-	(*circQueue).front = 0;
-	(*circQueue).back = 0;
+	//circQueue = (struct queue *) malloc (sizeof (struct queue));
+	circQueue -> array = (int*) malloc (size * sizeof(int)); //memory must be freed separately from struct
+	circQueue -> size = size;
+	circQueue -> front = 0;
+	circQueue -> back = 0;
 }
 
 void enqueue (struct queue *circQueue, int val){
-	if (((*circQueue).back + 1) % (*circQueue).size != (*circQueue).front){
-		(*circQueue).array[(*circQueue).back] = val;
-		(*circQueue).back = ((*circQueue).back + 1) % (*circQueue).size;
+	if ((circQueue -> back + 1) % circQueue -> size != circQueue -> front){
+		circQueue -> array[circQueue -> back] = val;
+		circQueue -> back = (circQueue -> back + 1) % circQueue -> size;
 	}
 }
 
 int dequeue (struct queue *circQueue){
-	if ((*circQueue).front != (*circQueue).back){
-		int front = (*circQueue).array[(*circQueue).front];
-		(*circQueue).front = ((*circQueue).front + 1) % (*circQueue).size;
+	if (circQueue -> front != circQueue -> back){
+		int front = circQueue -> array[circQueue -> front];
+		circQueue -> front = (circQueue -> front + 1) % circQueue -> size;
 		return front;
 	}
 	return 0;
+}
+
+void freeSpace (struct queue *circQueue){
+	free(circQueue -> array); //freeing allocated array memory
+	free(circQueue); //freeing allocated struct memory
 }
--- Circle Queue in C/queuetest.c
+++ Circle Queue in C/queuetest.c
@@ -3,19 +3,20 @@
 #include "circle_queue.c"
 
 int main () {
-	struct queue circleQueue;
-	makeQueue(&circleQueue, 4);
-	enqueue (&circleQueue, 3);
-	printf("%d   %d   %d\n", dequeue(&circleQueue), circleQueue.front, circleQueue.array[circleQueue.front]);
-	enqueue (&circleQueue, 6);
-	enqueue (&circleQueue, 5);
-	enqueue (&circleQueue, 2);
-	printf("%d   %d   %d\n", dequeue(&circleQueue), circleQueue.front, circleQueue.array[circleQueue.front]);
+	struct queue Queue;
+	struct queue *circleQueue;
+	circleQueue = &Queue;
+	makeQueue(circleQueue, 4);
+	enqueue (circleQueue, 3);
+	printf("%d   %d   %d\n", dequeue(circleQueue), circleQueue -> front, circleQueue -> array[circleQueue -> front]);
+	enqueue (circleQueue, 6);
+	enqueue (circleQueue, 5);
+	enqueue (circleQueue, 2);
+	printf("%d   %d   %d\n", dequeue(circleQueue), circleQueue -> front, circleQueue -> array[circleQueue -> front]);
 
-	printf("%d   %d   %d\n", dequeue(&circleQueue), circleQueue.front, circleQueue.array[circleQueue.front]);
-	enqueue (&circleQueue, 8);
-	printf("%d   %d   %d\n", dequeue(&circleQueue), circleQueue.front, circleQueue.array[circleQueue.front]);
-	printf("%d   %d   %d\n", dequeue(&circleQueue), circleQueue.front, circleQueue.array[circleQueue.front]);
-
-
+	printf("%d   %d   %d\n", dequeue(circleQueue), circleQueue -> front, circleQueue -> array[circleQueue -> front]);
+	enqueue (circleQueue, 8);
+	printf("%d   %d   %d\n", dequeue(circleQueue), circleQueue -> front, circleQueue -> array[circleQueue -> front]);
+	printf("%d   %d   %d\n", dequeue(circleQueue), circleQueue -> front, circleQueue -> array[circleQueue -> front]);
+	freeSpace (circleQueue);
 }
